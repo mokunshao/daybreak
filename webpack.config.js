33445e43e55bc55a5ac5e2442126bf5b01c0730d
mokunshao <mokunshao@foxmail.com>
2019-08-23 00:39:38 +0800

    修改 webpack 配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
-// const path = require('path');
+const path = require('path');
+
 module.exports = {
+  mode: 'production',
   entry: {
     index: './lib/index.tsx',
   },
   output: {
-    // path:''
+    path: path.resolve(__dirname, 'dist/lib'),
+    library: 'lillies',
+    libraryTarget: 'umd',
   },
   module: {
     rules: [
