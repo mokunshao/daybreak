2c54fca6ab724474e00851c54c3770df82eadf52
mokunshao <mokunshao@foxmail.com>
2019-09-10 22:39:22 +0800

    Update icon.tsx

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './icon.scss';
+import classes from './utils/classes';
 
+// 引入当前目录下 icon 文件夹的所有 svg 文件
 const importAllIcons = () => {
   const requireContext = require.context('./icons', true, /\.svg$/);
   requireContext.keys().forEach(requireContext);
@@ -10,14 +12,14 @@ importAllIcons();
 
 type iconName = 'qq' | 'wechat' | 'alipay';
 
-interface IconProps {
+interface IconProps extends React.SVGAttributes<SVGElement> {
   name: string & iconName;
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
-  const { name } = props;
+  const { name, className, ...res } = props;
   return (
-    <svg className="kaka-icon">
+    <svg className={classes('kaka-icon', className)} {...res}>
       <use xlinkHref={`#${name}`} />
     </svg>
   );
