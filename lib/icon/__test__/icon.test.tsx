080b6772d16cb666b1b8e0b5f38beacaf45878ba
mokunshao <mokunshao@foxmail.com>
2019-09-12 22:58:08 +0800

    Update icon.test.tsx

--- lib/icon/__test__/icon.test.tsx
+++ lib/icon/__test__/icon.test.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
 import Icon from '../icon';
 
 describe('Icon', () => {
   it('是个 svg', () => {
     const json = renderer.create(<Icon name="qq" />).toJSON();
     expect(json).toMatchSnapshot();
+  });
+  it('onClick', () => {
+    const fn = jest.fn();
+    const icon = mount(<Icon name="qq" onClick={fn} />);
+    icon.find('svg').simulate('click');
+    expect(fn).toBeCalled();
   });
 });
