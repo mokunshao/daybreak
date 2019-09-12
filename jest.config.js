a9d1f683fa40ecdd68983373827bcb436d2735d6
mokunshao <mokunshao@foxmail.com>
2019-09-12 22:58:11 +0800

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   verbose: true,
   transformIgnorePatterns: ['node_modules'],
   moduleNameMapper: {
     '^.+\\.(css|scss)$': '<rootDir>/mocks/styleMock.tsx',
     '^.+\\.(gif|ttf|eot|svg|png)$': '<rootDir>/mocks/fileMock.tsx',
   },
+  setupFilesAfterEnv: ['<rootDir>setupTests.tsx'],
 };
