164efe6bc707ccf1592cdb95d57926330cd1ea66
mokunshao <mokunshao@foxmail.com>
2019-09-23 00:21:58 +0800

    Update dialog.example.tsx

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Dialog from './dialog';
 
-const DialogExample: React.FunctionComponent = () => (
-  <div>dialog</div>
-);
+const DialogExample: React.FunctionComponent = () => {
+  const [x, setX] = useState(false);
+  return (
+    <div>
+      <button type="button" onClick={() => setX(!x)}>set</button>
+      <Dialog visible={x} />
+    </div>
+  );
+};
 
 export default DialogExample;
