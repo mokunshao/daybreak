fa623ff443d0c126e50db6d008ac5fbb4e07d0a8
mokunshao <mokunshao@foxmail.com>
2019-09-28 11:59:14 +0800

    Update dialog.tsx

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import './dialog.scss';
+import { jc } from '../utils/joinedClasses';
 
 interface Props {
   visible: boolean
 }
+
+const dialog = jc('dialog');
 
 const Dialog: React.FunctionComponent<Props> = (props) => {
   const { visible, children } = props;
   return (
     visible ? (
       <>
-        <div className="daybreak-dialog-mask" />
-        <div className="daybreak-dialog">
+        <div className={dialog('mask')} />
+        <div className={dialog()}>
           <div>
-            <span className="daybreak-dialog-close">X</span>
+            <span className={dialog('close')}>X</span>
           </div>
+          <div>{dialog('home')}</div>
           {children}
         </div>
       </>
