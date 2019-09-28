5080e8fcc469302d8e870fb3047353caa8c266ad
mokunshao <mokunshao@foxmail.com>
2019-09-29 02:45:29 +0800

    Update dialog.example.tsx

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -3,22 +3,43 @@ import Dialog from './dialog';
 
 const DialogExample: React.FunctionComponent = () => {
   const [x, setX] = useState(false);
+  const [y, setY] = useState(false);
   return (
-    <div>
-      <button type="button" onClick={() => setX(!x)}>set</button>
-      <Dialog
-        visible={x}
-        buttons={
-          [
-            <button type="button" onClick={() => setX(false)}>Yes</button>,
-            <button type="button" onClick={() => setX(false)}>No</button>,
-          ]
-        }
-        onClose={() => setX(false)}
-      >
-        hello
-      </Dialog>
-    </div>
+    <>
+      <div>
+        <h2>example 1</h2>
+        <button type="button" onClick={() => setX(!x)}>set</button>
+        <Dialog
+          visible={x}
+          buttons={
+            [
+              <button type="button" onClick={() => setX(false)}>Yes</button>,
+              <button type="button" onClick={() => setX(false)}>No</button>,
+            ]
+          }
+          onClose={() => setX(false)}
+        >
+          hello
+        </Dialog>
+      </div>
+      <div>
+        <h2>example 2</h2>
+        <button type="button" onClick={() => setY(!y)}>set</button>
+        <Dialog
+          visible={y}
+          buttons={
+            [
+              <button type="button" onClick={() => setY(false)}>Yes</button>,
+              <button type="button" onClick={() => setY(false)}>No</button>,
+            ]
+          }
+          onClose={() => setY(false)}
+          closeOnClickMask={false}
+        >
+          hello
+        </Dialog>
+      </div>
+    </>
   );
 };
 
