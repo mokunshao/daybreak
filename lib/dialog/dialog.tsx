5cc4a562facf7e36991269f0e3c183e54f2d8806
mokunshao <mokunshao@foxmail.com>
2019-09-28 14:57:00 +0800

    Update dialog.tsx

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -4,22 +4,29 @@ import { jc } from '../utils/joinedClasses';
 
 interface Props {
   visible: boolean
+  title?: string
 }
 
 const dialog = jc('dialog');
 
 const Dialog: React.FunctionComponent<Props> = (props) => {
-  const { visible, children } = props;
+  const { visible, title, children } = props;
   return (
     visible ? (
       <>
         <div className={dialog('mask')} />
         <div className={dialog()}>
-          <div>
-            <span className={dialog('close')}>X</span>
+          <div className={dialog('header')}>
+            <span className={dialog('title')}>{title}</span>
+            <button type="button" className={dialog('close')}>X</button>
           </div>
-          <div>{dialog('home')}</div>
-          {children}
+          <div className={dialog('main')}>
+            {children}
+          </div>
+          <div className={dialog('footer')}>
+            <button type="button">Yes</button>
+            <button type="button">No</button>
+          </div>
         </div>
       </>
     ) : null
