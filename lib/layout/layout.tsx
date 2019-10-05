b5e31f46708a8c57cc526b3bf73a421e936fd6d9
mokunshao <mokunshao@foxmail.com>
2019-10-05 23:28:10 +0800

    Update layout.tsx

--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './layout.scss';
 import { jc } from '../utils/joinedClasses';
+import classes from '../utils/classes';
 
 const layout = jc('layout');
 
 interface Props extends React.HtmlHTMLAttributes<HTMLElement> { }
 
-const Layout: React.FunctionComponent<Props> = () => (
-  <div className={layout()}>layout</div>
-);
+const Layout: React.FunctionComponent<Props> = (props) => {
+  const { className, children, ...rest } = props;
+  return (
+    <div className={classes(layout(), className)} {...rest}>{children}</div>
+  );
+};
 
 export default Layout;
