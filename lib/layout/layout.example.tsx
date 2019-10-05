8debba1823025f98f4c6c47d4980c1cdd734e53c
mokunshao <mokunshao@foxmail.com>
2019-10-05 23:28:06 +0800

    Update layout.tsx

--- lib/layout/layout.example.tsx
+++ lib/layout/layout.example.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Layout from './layout';
+import Header from './header';
+import Content from './content';
+import Footer from './footer';
 
 export default function () {
   return (
     <>
       <div>
         <h2>example 1</h2>
-        <Layout />
+        <Layout>
+          <Header>header</Header>
+          <Content>content</Content>
+          <Footer>footer</Footer>
+        </Layout>
       </div>
     </>
   );
