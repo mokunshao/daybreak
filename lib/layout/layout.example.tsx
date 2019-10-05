df09d7633bb822617dd264e19d57a35f69651c5b
mokunshao <mokunshao@foxmail.com>
2019-10-05 18:14:05 +0800

    Create layout.example.tsx

---
+++ lib/layout/layout.example.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from './layout';
+
+export default function () {
+  return (
+    <>
+      <div>
+        <h2>example 1</h2>
+        <Layout />
+      </div>
+    </>
+  );
+}
