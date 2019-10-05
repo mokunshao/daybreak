b6907c0d70dc24878d4860b3fb9b592e4734db15
mokunshao <mokunshao@foxmail.com>
2019-10-06 02:33:55 +0800

    Update scopedClass.tsx

--- lib/utils/scopedClass.tsx
+++ lib/utils/scopedClass.tsx
@@ -2,13 +2,13 @@ import classes from './classes';
 import { joinedClasses } from './joinedClasses';
 
 interface Options {
-  extra: string | (string | undefined)[]
+  extra: string
 }
 
 const scopedClass = (prefix: string) => {
   const joinedClasseMaker = joinedClasses(prefix);
-  return function x(name: string | (string | undefined)[], options: Options) {
-    return classes(joinedClasseMaker(...name), ...options.extra);
+  return function classMaker(name: string, options: Options) {
+    return classes(joinedClasseMaker(name), options.extra);
   };
 };
 
