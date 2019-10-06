6003b87dec49198f05a744cc3af2c2e5a3ca27e5
mokunshao <mokunshao@foxmail.com>
2019-10-06 17:53:38 +0800

    Update layout.tsx

--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -2,15 +2,24 @@ import React from 'react';
 import './layout.scss';
 import joinedClass from '../utils/joinedClass';
 import classes from '../utils/classes';
+import Aside from './aside';
 
 const layout = joinedClass('layout');
 
-interface Props extends React.HtmlHTMLAttributes<HTMLElement> { }
+interface Props extends React.HtmlHTMLAttributes<HTMLElement> {
+  children: React.ReactElement | Array<React.ReactElement>
+}
 
 const Layout: React.FunctionComponent<Props> = (props) => {
+  let hasAside = false;
   const { className, children, ...rest } = props;
+  if (Array.isArray(children)) {
+    (children as Array<React.ReactElement>).forEach((element) => {
+      element.type === Aside && (hasAside = true);
+    });
+  }
   return (
-    <div className={classes(layout(), className)} {...rest}>{children}</div>
+    <div className={classes(layout(), className, hasAside ? 'has-aside' : '')} {...rest}>{children}</div>
   );
 };
 
