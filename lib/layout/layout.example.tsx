a4e1e814476c7c566f106148d3bfe206a1edcbe8
mokunshao <mokunshao@foxmail.com>
2019-10-06 15:27:31 +0800

    Update layout.example.tsx

--- lib/layout/layout.example.tsx
+++ lib/layout/layout.example.tsx
@@ -3,15 +3,27 @@ import Layout from './layout';
 import Header from './header';
 import Content from './content';
 import Footer from './footer';
+import Aside from './aside';
 
 export default function () {
   return (
     <>
       <div>
         <h2>example 1</h2>
-        <Layout>
+        <Layout style={{ height: 300 }}>
           <Header>header</Header>
           <Content>content</Content>
+          <Footer>footer</Footer>
+        </Layout>
+      </div>
+      <div>
+        <h2>example 2</h2>
+        <Layout style={{ height: 300 }}>
+          <Header>header</Header>
+          <Layout>
+            <Aside>aside</Aside>
+            <Content>content</Content>
+          </Layout>
           <Footer>footer</Footer>
         </Layout>
       </div>
