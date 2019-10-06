315833a1a441f6f6c3e5159531ca4797ec40d52c
mokunshao <mokunshao@foxmail.com>
2019-10-06 15:27:42 +0800

    Delete joinedClasses.tsx

--- lib/utils/joinedClasses.tsx
+++
@@ -1,9 +0,0 @@
-function joinedClasses(prefix: string = '') {
-  return function classesHandler(...names: (string | undefined)[]) {
-    return ['daybreak', prefix, ...names].filter(Boolean).join('-');
-  };
-}
-
-const jc = joinedClasses;
-
-export { joinedClasses, jc };
