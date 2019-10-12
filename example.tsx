c83d1fe883c30f921a5101b71bb100726b3dc063
mokunshao <mokunshao@foxmail.com>
2019-10-12 20:56:42 +0800

    Update example.tsx

--- example.tsx
+++ example.tsx
@@ -1,40 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, NavLink } from 'react-router-dom';
 import IconExample from './lib/icon/icon.example';
 import DialogExample from './lib/dialog/dialog.example';
 import LayoutExample from './lib/layout/layout.example';
 import './example.scss';
 import {
-  Layout, Header, Aside, Content, Footer,
+  Layout, Header, Aside, Content,
 } from './lib/layout/layout';
 
 ReactDOM.render((
   <Router>
-    <Layout>
-      <Header><div className="logo">Daybreak</div></Header>
+    <Layout className="site-page">
+      <Header className="site-header"><div className="logo">Daybreak</div></Header>
       <Layout>
-        <Aside>
-          <h2>组件</h2>
-          <ul>
-            <li>
-              <Link to="/icon">Icon</Link>
-            </li>
-            <li>
-              <Link to="/dialog">Dialog</Link>
-            </li>
-            <li>
-              <Link to="/layout">Layout</Link>
-            </li>
-          </ul>
+        <Aside className="site-aside">
+          <NavLink className="site-aside-link" to="/icon">Icon</NavLink>
+          <NavLink className="site-aside-link" to="/dialog">Dialog</NavLink>
+          <NavLink className="site-aside-link" to="/layout">Layout</NavLink>
         </Aside>
-        <Content>
+        <Content className="site-content">
           <Route path="/icon" component={IconExample} />
           <Route path="/dialog" component={DialogExample} />
           <Route path="/layout" component={LayoutExample} />
         </Content>
       </Layout>
-      <Footer>2019</Footer>
     </Layout>
   </Router>
 ), document.querySelector('#root'));
