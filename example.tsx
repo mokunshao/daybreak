9073fa498e9df09978fa0af263bbe216b1bd8f77
mokunshao <mokunshao@foxmail.com>
2019-10-13 02:39:07 +0800

    Update example.tsx

--- example.tsx
+++ example.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router, Route, NavLink } from 'react-router-dom';
-import IconExample from './lib/icon/icon.example';
+import IconDemo from './lib/icon/icon.demo';
 import DialogExample from './lib/dialog/dialog.example';
 import LayoutExample from './lib/layout/layout.example';
 import './example.scss';
@@ -20,7 +20,7 @@ ReactDOM.render((
           <NavLink className="site-aside-link" to="/layout">Layout</NavLink>
         </Aside>
         <Content className="site-content">
-          <Route path="/icon" component={IconExample} />
+          <Route path="/icon" component={IconDemo} />
           <Route path="/dialog" component={DialogExample} />
           <Route path="/layout" component={LayoutExample} />
         </Content>
