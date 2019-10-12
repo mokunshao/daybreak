267f4019d8ae0900dbd0e7b9d336504f62d61c91
mokunshao <mokunshao@foxmail.com>
2019-10-12 14:24:42 +0800

    Update scopedClass.tsx

--- lib/utils/scopedClass.tsx
+++ lib/utils/scopedClass.tsx
@@ -6,20 +6,14 @@ interface Toggle {
   [key: string]: boolean
 }
 
-const scopedClassMaker = (prefix: string) => function x(name?: string | Toggle, option?: Options) {
-  let name2;
-  let result;
-  if (typeof name === 'string' || name === undefined) {
-    name2 = name;
-    result = [prefix, name2].filter(Boolean).join('-');
-  } else {
-    name2 = Object.entries(name).filter((v) => v[1]).map((v) => v[0]);
-    result = name2.map((n) => [prefix, n].filter(Boolean).join('-')).join(' ');
-  }
-  if (option && option.extra) {
-    return [result, option.extra].filter(Boolean).join(' ');
-  }
-  return result;
-};
+const scopedClassMaker = (prefix: string) => (
+  name: string | Toggle,
+  option?: Options,
+) => Object.entries(name instanceof Object ? name : { [name]: true })
+  .filter((v) => v[1])
+  .map((v) => v[0])
+  .map((n) => [prefix, n].filter(Boolean).join('-'))
+  .concat((option && option.extra) || [])
+  .join(' ');
 
 export default scopedClassMaker;
