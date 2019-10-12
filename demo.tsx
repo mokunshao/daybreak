a9788b1d36931ad40c9c68244407f430c8eb91f5
mokunshao <mokunshao@foxmail.com>
2019-10-13 03:26:40 +0800

    添加代码高亮

--- demo.tsx
+++ demo.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Highlight, { defaultProps } from 'prism-react-renderer';
 
 interface Props {
   code: string
@@ -9,9 +10,31 @@ const Demo: React.FunctionComponent<Props> = (props) => {
   return (
     <>
       {children}
-      <pre>
-        {code}
-      </pre>
+      <Highlight {...defaultProps} code={code} language="jsx">
+        {
+          ({
+            className,
+            style,
+            tokens,
+            getLineProps,
+            getTokenProps,
+          }) => (
+            <pre className={className} style={style}>
+              {
+                tokens.map((line, i) => (
+                  <div {...getLineProps({ line, key: i })}>
+                    {
+                      line.map((token, key) => (
+                        <span {...getTokenProps({ token, key })} />
+                      ))
+                    }
+                  </div>
+                ))
+              }
+            </pre>
+          )
+        }
+      </Highlight>
     </>
   );
 };
