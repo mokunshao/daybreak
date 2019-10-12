53d59a29c6506360c1d0447c1acacfb118658edb
mokunshao <mokunshao@foxmail.com>
2019-10-12 13:57:25 +0800

    Update scopedClass.tsx

--- lib/utils/scopedClass.tsx
+++ lib/utils/scopedClass.tsx
@@ -1,15 +1,25 @@
-import classes from './classes';
-import joinedClass from './joinedClass';
-
 interface Options {
-  extra: string
+  extra: string | undefined
 }
 
-const scopedClass = (prefix: string) => {
-  const joinedClasseMaker = joinedClass(prefix);
-  return function classMaker(name: string, options: Options) {
-    return classes(joinedClasseMaker(name), options.extra);
-  };
+interface Toggle {
+  [key: string]: boolean
+}
+
+const scopedClassMaker = (prefix: string) => function x(name?: string | Toggle, option?: Options) {
+  let name2;
+  let result;
+  if (typeof name === 'string' || name === undefined) {
+    name2 = name;
+    result = [prefix, name2].filter(Boolean).join('-');
+  } else {
+    name2 = Object.entries(name).filter((v) => v[1]).map((v) => v[0]);
+    result = name2.map((n) => [prefix, n].filter(Boolean).join('-')).join(' ');
+  }
+  if (option && option.extra) {
+    return [result, option.extra].filter(Boolean).join(' ');
+  }
+  return result;
 };
 
-export default scopedClass;
+export default scopedClassMaker;
