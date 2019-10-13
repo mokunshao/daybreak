7c64cc3d07ce99ae0ef6d31bd885cc2589794b1f
mokunshao <mokunshao@foxmail.com>
2019-10-13 14:55:39 +0800

    Update demo.tsx

--- demo.tsx
+++ demo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
 
 interface Props {
@@ -6,35 +6,44 @@ interface Props {
 }
 
 const Demo: React.FunctionComponent<Props> = (props) => {
+  const [codeVisible, setCodeVisible] = useState(false);
   const { code, children } = props;
+  const hightLightCode = (
+    <Highlight {...defaultProps} code={code} language="jsx">
+      {
+        ({
+          className,
+          style,
+          tokens,
+          getLineProps,
+          getTokenProps,
+        }) => (
+          <pre className={className} style={style}>
+            {
+              tokens.map((line, i) => (
+                <div {...getLineProps({ line, key: i })}>
+                  {
+                    line.map((token, key) => (
+                      <span {...getTokenProps({ token, key })} />
+                    ))
+                  }
+                </div>
+              ))
+            }
+          </pre>
+        )
+      }
+    </Highlight>
+  );
   return (
     <>
-      {children}
-      <Highlight {...defaultProps} code={code} language="jsx">
-        {
-          ({
-            className,
-            style,
-            tokens,
-            getLineProps,
-            getTokenProps,
-          }) => (
-            <pre className={className} style={style}>
-              {
-                tokens.map((line, i) => (
-                  <div {...getLineProps({ line, key: i })}>
-                    {
-                      line.map((token, key) => (
-                        <span {...getTokenProps({ token, key })} />
-                      ))
-                    }
-                  </div>
-                ))
-              }
-            </pre>
-          )
-        }
-      </Highlight>
+      <div>
+        {children}
+      </div>
+      <div>
+        <button type="button" onClick={() => setCodeVisible(!codeVisible)}>查看代码</button>
+      </div>
+      {codeVisible && hightLightCode}
     </>
   );
 };
