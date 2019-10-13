d2e62a1ec943defc5cae83a0815dbb55db730f8e
mokunshao <mokunshao@foxmail.com>
2019-10-13 17:03:41 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    daybreak: './example.tsx',
+    daybreak: './lib/index.tsx',
   },
   output: {
     path: path.resolve(__dirname, 'dist/lib'),
