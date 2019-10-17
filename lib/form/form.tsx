790ebc2b83af98361707d36bf6f28ab1072ec290
mokunshao <mokunshao@foxmail.com>
2019-10-17 18:15:14 +0800

    Update form.tsx

--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -1,5 +1,26 @@
 import React from 'react';
 
-const Form:React.FunctionComponent = () => <div>form</div>;
+interface Props {
+  value: { [K: string]: any }
+  fields: Array<{ name: string, label: string, input: { type: string } }>
+  buttons: React.ReactFragment
+}
+
+const Form: React.FunctionComponent<Props> = (props) => {
+  const { fields, buttons } = props;
+  return (
+    <form>
+      {fields.map((item) => (
+        <div key={item.name}>
+          {item.label}
+          <input type={item.input.type} />
+        </div>
+      ))}
+      <div>
+        {buttons}
+      </div>
+    </form>
+  );
+};
 
 export default Form;
