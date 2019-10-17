8a39ea07b4bba8b589e6aa3b6de95174bfbcf773
mokunshao <mokunshao@foxmail.com>
2019-10-17 18:15:11 +0800

    Update form.example.tsx

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -1,4 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from './form';
 
-export default () => <div><Form /></div>;
+export default () => {
+  const [formData] = useState({
+    username: '',
+    password: '',
+  });
+  const [fields] = useState([
+    { name: 'username', label: '用户名', input: { type: 'text' } },
+    { name: 'password', label: '密码', input: { type: 'password' } },
+  ]);
+  return (
+    <div>
+      <Form
+        value={formData}
+        fields={fields}
+        buttons={
+          (
+            <>
+              <button type="submit">提交</button>
+              <button type="button">返回</button>
+            </>
+          )
+        }
+      />
+    </div>
+  );
+};
