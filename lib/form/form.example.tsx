e107bc9527349045a32523fb51258b1b4741a7c0
mokunshao <mokunshao@foxmail.com>
2019-10-17 23:23:15 +0800

    Update form.example.tsx

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
-import Form from './form';
+import Form, { FormValue } from './form';
 
 export default () => {
-  const [formData] = useState({
-    username: '',
-    password: '',
+  const [formData, setFormData] = useState<FormValue>({
+    username: 'fafa',
+    password: 'qqq',
   });
   const [fields] = useState([
     { name: 'username', label: '用户名', input: { type: 'text' } },
     { name: 'password', label: '密码', input: { type: 'password' } },
   ]);
+  const onSubmit = () => console.log(formData);
+  const onChange = (data: FormValue) => setFormData(data);
   return (
     <div>
       <Form
@@ -23,6 +25,8 @@ export default () => {
             </>
           )
         }
+        onSubmit={onSubmit}
+        onChange={onChange}
       />
     </div>
   );
