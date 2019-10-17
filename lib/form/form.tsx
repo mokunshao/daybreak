34d22ee9fbb15d827a393c60f4b9fb962f43abc9
mokunshao <mokunshao@foxmail.com>
2019-10-17 23:23:19 +0800

    Update form.tsx

--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -1,19 +1,43 @@
 import React from 'react';
 
+export interface FormValue {
+  [K: string]: any
+}
+
 interface Props {
-  value: { [K: string]: any }
+  value: FormValue
   fields: Array<{ name: string, label: string, input: { type: string } }>
   buttons: React.ReactFragment
+  onSubmit: React.FormEventHandler
+  onChange: (value: FormValue) => void
 }
 
 const Form: React.FunctionComponent<Props> = (props) => {
-  const { fields, buttons } = props;
+  const {
+    value,
+    fields,
+    buttons,
+    onSubmit,
+    onChange,
+  } = props;
+  const onSubmit2 = (e: React.FormEvent<Element>) => {
+    e.preventDefault();
+    onSubmit(e);
+  };
+  const onInputChange = (name: string, e: React.ChangeEvent<HTMLInputElement>) => {
+    const data = { ...value, [name]: e.target.value };
+    onChange(data);
+  };
   return (
-    <form>
+    <form onSubmit={onSubmit2}>
       {fields.map((item) => (
         <div key={item.name}>
           {item.label}
-          <input type={item.input.type} />
+          <input
+            type={item.input.type}
+            value={value[item.name]}
+            onChange={onInputChange.bind(null, item.name)}
+          />
         </div>
       ))}
       <div>
