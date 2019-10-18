a5f2602310e3b0fae416802822c18a48b09d97ce
mokunshao <mokunshao@foxmail.com>
2019-10-19 00:48:48 +0800

    Update joinedClass.tsx

--- lib/utils/joinedClass.tsx
+++ lib/utils/joinedClass.tsx
@@ -1,7 +1,9 @@
-function joinedClass(prefix: string = '') {
+function joinedClass(...prefix: (string | undefined)[]) {
   return function classesHandler(...names: (string | undefined)[]) {
-    return ['daybreak', prefix, ...names].filter(Boolean).join('-');
+    return ['daybreak', ...prefix, ...names].filter(Boolean).join('-');
   };
 }
 
 export default joinedClass;
+
+export { joinedClass };
