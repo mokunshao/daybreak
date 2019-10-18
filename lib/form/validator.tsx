90e01d79c7422c7fa9204960082acb577c8b1c8c
mokunshao <mokunshao@foxmail.com>
2019-10-18 17:12:49 +0800

    Update validator.tsx

--- lib/form/validator.tsx
+++ lib/form/validator.tsx
@@ -3,7 +3,7 @@ import { FormValues } from './form';
 interface FormRule {
   key: string
   require?: boolean
-  mixLength?: number
+  minLength?: number
   maxLength?: number
 }
 
@@ -13,15 +13,29 @@ interface FormErrors {
   [k: string]: string[]
 }
 
+function isEmpty(value: any) {
+  return value === null || value === undefined || value === '';
+}
+
 const Validator = (values: FormValues, rules: FormRules): FormErrors => {
   const errors: any = {};
+  const addError = (key: string, message: string) => {
+    if (errors[key] === undefined) {
+      errors[key] = [];
+    }
+    errors[key].push(message);
+  };
   rules.forEach((rule) => {
     const { key } = rule;
     const value = values[key];
-    if (rule.require) {
-      if (value === null || value === undefined || value === '') {
-        errors[key] = ['必填'];
-      }
+    if (rule.require && isEmpty(value)) {
+      addError(key, '必填');
+    }
+    if (rule.minLength && !isEmpty(value) && value.length < rule.minLength) {
+      addError(key, '太短');
+    }
+    if (rule.maxLength && !isEmpty(value) && value.length > rule.maxLength) {
+      addError(key, '太长');
     }
   });
   return errors;
