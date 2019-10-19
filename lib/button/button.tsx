8dfbf7c36a88571f2e08a3c4ccb8b26796cfa7d0
mokunshao <mokunshao@foxmail.com>
2019-10-19 22:46:02 +0800

    Update button.tsx

--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -5,15 +5,18 @@ import './button.scss';
 
 const button = joinedClass('button');
 
-interface Props extends ButtonHTMLAttributes<HTMLButtonElement> { }
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  mode?: 'normal' | 'primary' | 'danger'
+}
 
 const Button: React.FunctionComponent<Props> = (props) => {
   const {
+    mode = 'normal',
     className,
     children,
     ...rest
   } = props;
-  return <button type="button" className={classes(button(), className)} {...rest}>{children}</button>;
+  return <button type="button" className={classes(button(), button(mode), className)} {...rest}>{children}</button>;
 };
 
 export default Button;
