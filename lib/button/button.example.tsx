cfc1be073d351214a7bf5526fff0f9650c8644f9
mokunshao <mokunshao@foxmail.com>
2019-11-21 02:40:27 +0800

    写 button 文档

--- lib/button/button.example.tsx
+++ lib/button/button.example.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import { Button } from './button';
+import { Table } from '../table/table';
+
+const columns = ['Attribute', 'Description', 'Type', 'Accepted values', 'Default'];
+
+const data = [
+  [
+    'mode',
+    'mode',
+    'string',
+    '"normal"/"primary"/"danger"',
+    '"normal"',
+  ],
+];
 
 export default () => (
   <>
@@ -11,6 +24,10 @@ export default () => (
     </div>
     <div style={{ marginBottom: '0.5em' }}>
       <Button mode="danger">danger</Button>
+    </div>
+    <div>
+      <h2>Props</h2>
+      <Table columns={columns} data={data} />
     </div>
   </>
 );
