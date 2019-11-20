c66f0b94096f3a45074577dbbd864101e6e9657e
mokunshao <mokunshao@foxmail.com>
2019-11-21 04:40:05 +0800

    修改 demo 组件，移除代码高亮

--- demo.tsx
+++ demo.tsx
@@ -1,48 +1,21 @@
 import React, { useState } from 'react';
-import Highlight, { defaultProps } from 'prism-react-renderer';
 import { Button } from './lib/button/button';
 
 interface Props {
-  code: string
+  code: string;
 }
-
+const style = { background: '#eee', padding: '1em', display: 'inline-block' };
 const Demo: React.FunctionComponent<Props> = (props) => {
   const [codeVisible, setCodeVisible] = useState(false);
   const { code, children } = props;
-  const hightLightCode = (
-    <Highlight {...defaultProps} code={code} language="jsx">
-      {
-        ({
-          className,
-          style,
-          tokens,
-          getLineProps,
-          getTokenProps,
-        }) => (
-          <pre className={className} style={style}>
-            {
-              tokens.map((line, i) => (
-                <div {...getLineProps({ line, key: i })}>
-                  {
-                    line.map((token, key) => (
-                      <span {...getTokenProps({ token, key })} />
-                    ))
-                  }
-                </div>
-              ))
-            }
-          </pre>
-        )
-      }
-    </Highlight>
-  );
+  const hightLightCode = <pre style={style}><code>{code}</code></pre>;
   return (
     <>
       <div>
         {children}
       </div>
       <div>
-        <Button type="button" onClick={() => setCodeVisible(!codeVisible)}>查看代码</Button>
+        <Button style={{ margin: '0.5em 0' }} type="button" onClick={() => setCodeVisible(!codeVisible)}>Code Preview</Button>
       </div>
       {codeVisible && hightLightCode}
     </>
--- lib/icon/icon.demo.tsx
+++ lib/icon/icon.demo.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import IconExample from './icon.example';
 import Demo from '../../demo';
 
+const code = `import React from 'react';
+import {Icon} from 'daybreak';
+const IconExample: React.FunctionComponent = () => (
+  <div>
+    <Icon name="qq" />
+    <Icon name="alipay" />
+    <Icon name="wechat" />
+  </div>
+);
+export default IconExample;`;
+
 const IconDemo = () => (
-  <Demo code={require('!!raw-loader!./icon.example.tsx').default}>
+  // <Demo code={require('!!raw-loader!./icon.example.tsx').default}>
+  <Demo code={code}>
     <IconExample />
   </Demo>
 );
