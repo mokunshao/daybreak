f8fcf285e4e9b62ad7d1d9121b995526551097ed
mokunshao <mokunshao@foxmail.com>
2019-11-22 22:03:23 +0800

    完善 checkbox

--- lib/checkbox/checkbox.example.tsx
+++ lib/checkbox/checkbox.example.tsx
@@ -7,11 +7,11 @@ export const CheckboxExample: React.FC = () => {
   const [checked3, setChecked3] = useState(false);
   return (
     <div>
-      <Checkbox checked={checked1} change={(value) => setChecked1(value)} />
+      <Checkbox checked={checked1} onChange={(e) => setChecked1(e.target.checked)} />
       <br />
-      <Checkbox checked={checked2} change={(value) => setChecked2(value)} />
+      <Checkbox checked={checked2} onChange={(e) => setChecked2(e.target.checked)} />
       <br />
-      <Checkbox checked={checked3} change={(value) => setChecked3(value)}>Agree?</Checkbox>
+      <Checkbox checked={checked3} onChange={(e) => setChecked3(e.target.checked)}>Agree?</Checkbox>
     </div>
   );
 };
--- lib/checkbox/checkbox.tsx
+++ lib/checkbox/checkbox.tsx
@@ -1,22 +1,22 @@
-import React, { HtmlHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes } from 'react';
 import './checkbox.scss';
 import { classes } from '../utils/classes';
 import { joinedClass } from '../utils/joinedClass';
 
 const baseClass = joinedClass('checkbox');
 
-interface Props extends HtmlHTMLAttributes<HTMLLabelElement> {
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   checked: boolean;
-  change: (value: boolean) => void;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const Checkbox: React.FC<Props> = (props) => {
   const {
-    checked, change, children, className, ...rest
+    checked, onChange, children, className, ...rest
   } = props;
   return (
-    <label className={classes(baseClass(), className)} {...rest}>
-      <input className={baseClass('input')} type="checkbox" checked={checked} onChange={(e) => change(e.target.checked)} />
+    <label className={classes(baseClass(), className)}>
+      <input className={baseClass('input')} type="checkbox" checked={checked} onChange={(e) => onChange(e)} {...rest} />
       <span className={baseClass('checkmark')} />
       {children && <div className={baseClass('children')}>{children}</div>}
     </label>
