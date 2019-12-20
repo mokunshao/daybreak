53fcfd56f29396aa60e4341e6bc499f5cbae49e3
mokunshao <mokunshao@foxmail.com>
2019-12-20 19:32:00 +0800

    修改导出的文件

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,4 +1,36 @@
-// export { default as Icon } from './icon/icon';
+import Button from './button/button';
+import Checkbox from './checkbox/checkbox';
+import Dialog from './dialog/dialog';
+import Form from './form/form';
 import Icon from './icon/icon';
+import Input from './input/input';
+import Layout from './layout/layout';
+import Header from './layout/header';
+import Footer from './layout/footer';
+import Content from './layout/content';
+import Aside from './layout/aside';
+import Radio from './radio/radio';
+import RadioGroup from './radio/radio-group';
+import Switch from './switch/switch';
 
-export default Icon;
+import CodePreview from './code-preview/code-preview';
+import Table from './table/table';
+
+export {
+  Button,
+  Checkbox,
+  Dialog,
+  Form,
+  Icon,
+  Input,
+  Layout,
+  Header,
+  Footer,
+  Content,
+  Aside,
+  Radio,
+  RadioGroup,
+  Switch,
+  CodePreview,
+  Table,
+};
