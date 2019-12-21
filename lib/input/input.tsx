529778cada3e001b7cc4ef8baf769cf721f2be60
mokunshao <mokunshao@foxmail.com>
2019-12-21 15:59:47 +0800

    input 的 clearable 功能实现

--- lib/input/input.tsx
+++ lib/input/input.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { classes } from '../utils/classes';
 import { joinedClass } from '../utils/joinedClass';
 import './input.scss';
@@ -8,14 +8,34 @@ const baseClass = joinedClass('input');
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement>{
   clearable?: boolean;
+  onClear?: Function;
 }
 
 const Input: React.FunctionComponent<Props> = (props) => {
-  const { className, clearable, ...rest } = props;
+  const inputElement = useRef<HTMLInputElement>(null);
+  const {
+    className, clearable, onClear, ...rest
+  } = props;
+
+  function clear() {
+    const { current } = inputElement;
+    if (!current) return;
+    current.value = '';
+    if (onClear) onClear();
+  }
+
+  function showClearIcon() {
+    if (!clearable) return null;
+    const { current } = inputElement;
+    if (!current) return null;
+    if (!current.value) return null;
+    return <Icon name="clear" className={baseClass('clear')} onClick={clear} />;
+  }
+
   return (
     <div className={baseClass('wrapper')}>
-      <input className={classes(baseClass(), className)} {...rest} />
-      {clearable && <Icon name="clear" className={baseClass('clear')} />}
+      <input ref={inputElement} className={classes(baseClass(), className)} {...rest} />
+      {showClearIcon()}
     </div>
   );
 };
--- site/examples/input.example.tsx
+++ site/examples/input.example.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../../lib/input/input';
 import Table from '../../lib/table/table';
 
@@ -16,17 +16,19 @@ const data = [
 ];
 
 export default function () {
+  const [value, setValue] = useState('');
+  const [value2, setValue2] = useState('');
   return (
     <div>
       <h1>Input</h1>
       <h2>Example</h2>
       <h3>Basic Usage</h3>
       <div>
-        <Input />
+        <Input value={value} onChange={(e) => setValue(e.target.value)} />
       </div>
       <h3>Clearable</h3>
       <div>
-        <Input clearable />
+        <Input clearable value={value2} onChange={(e) => setValue2(e.target.value)} />
       </div>
       <h2>Props</h2>
       <Table columns={columns} data={data} />
