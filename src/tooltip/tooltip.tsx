a06c92de122bcc1913fba013b57e3c9c3558e37b
mokunshao <mokunshao@foxmail.com>
2019-12-24 16:45:22 +0800

    完善 tooltip

--- src/tooltip/tooltip.tsx
+++ src/tooltip/tooltip.tsx
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { HTMLProps, ReactNode, useState } from 'react';
+import ReactDOM from 'react-dom';
+import { joinedClass } from '../utils/joinedClass';
+import { classes } from '../utils/classes';
 
-const Tooltip = () => <div>tooltip</div>;
+const baseClass = joinedClass('tooltip');
+
+// interface Props extends HTMLProps<HTMLElement>{
+//   render?: ReactNode;
+// }
+interface Props extends HTMLProps<HTMLElement>{
+  render?: ReactNode;
+}
+
+function ToolipItem() {
+  return ReactDOM.createPortal(<div>ToolipItem</div>, document.body);
+}
+
+
+const Tooltip: React.FC<Props> = (props) => {
+  const {
+    children, className, render, ...rest
+  } = props;
+
+  const [tooltipVisible, setTooltipVisible] = useState(false);
+
+  function showTooltip() {
+    setTooltipVisible(true);
+  }
+
+  return (
+    <>
+      <span className={classes(baseClass(), className)} onClick={showTooltip} {...rest}>
+        {children}
+      </span>
+      {tooltipVisible && <ToolipItem />}
+    </>
+  );
+};
 
 export default Tooltip;
