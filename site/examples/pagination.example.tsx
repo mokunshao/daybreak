e321d7b33fc7deb95f8ae7ea78f3aef7fb084159
mokunshao <mokunshao@foxmail.com>
2019-12-30 14:06:56 +0800

    完善 pagination

--- site/examples/pagination.example.tsx
+++ site/examples/pagination.example.tsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import { Pagination } from '../../src/pagination';
 
 export default function () {
-  const [current, setCurrent] = useState(0);
-  const onChange = (n) => setCurrent(n);
+  const [current, setCurrent] = useState(1);
+  const onChange = (n: number) => setCurrent(n);
   return (
     <div>
       <Pagination pageSize={10} total={100} current={current} onChange={onChange} />
--- src/pagination/pagination.tsx
+++ src/pagination/pagination.tsx
@@ -1,5 +1,5 @@
 import React, {
-  HTMLProps, useMemo, MouseEventHandler, useState,
+  HTMLProps, useMemo, useCallback,
 } from 'react';
 import { joinedClass } from '../utils/joinedClass';
 import { classes } from '../utils/classes';
@@ -13,7 +13,7 @@ interface Props extends Omit<HTMLProps<HTMLDivElement>, 'onChange'> {
   current: number;
   pageSize: number;
   total: number;
-  onChange: Function;
+  onChange: (n: number) => void;
   disabled?: boolean;
 }
 
@@ -21,39 +21,54 @@ const Pagination: React.FC<Props> = (props) => {
   const {
     className, total, pageSize, current, onChange,
   } = props;
-  const pagesCount = useMemo(() => total / pageSize, [total, pageSize]);
-  const arr = [];
-  for (let index = 0; index < pagesCount; index += 1) {
-    arr.push(index);
-  }
 
-  const handleClick = (n: number) => {
+  const pagesCount = useMemo(() => total / pageSize, [total, pageSize]);
+
+  const arr = useMemo(() => {
+    const array = [];
+    for (let index = 0; index < pagesCount; index += 1) {
+      array.push(index + 1);
+    }
+    return array;
+  }, [pagesCount]);
+
+  const handleClick = useCallback((n: number) => {
+    if (n < 1) return;
+    if (n > arr.length) return;
     onChange(n);
-  };
+  }, [arr.length, onChange]);
+
   return (
     <div className={classes(baseClass(), className)}>
       <Button
+        disabled={current === 1}
         className={baseClass('button')}
         type="button"
         onClick={() => handleClick(current - 1)}
       >
         <Icon name="left" />
       </Button>
-      {arr.map((item) => (
-        <Button
-          className={baseClass('button')}
-          type="button"
-          key={item}
-          mode={current === item ? 'primary' : 'normal'}
-          onClick={() => handleClick(item)}
-        >
-          {item}
-        </Button>
-      ))}
-      <Button className={baseClass('button')} type="button">
+      {
+        arr.map((item) => (
+          <Button
+            className={baseClass('button')}
+            type="button"
+            key={item}
+            mode={current === item ? 'primary' : 'normal'}
+            onClick={() => handleClick(item)}
+          >
+            {item}
+          </Button>
+        ))
+      }
+      <Button
+        disabled={current === arr.length}
+        className={baseClass('button')}
+        type="button"
+        onClick={() => handleClick(current + 1)}
+      >
         <Icon
           name="right"
-          onClick={() => handleClick(current + 1)}
         />
       </Button>
     </div>
