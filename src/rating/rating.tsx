42cf566c454233af81300f1d61bad3d61a12831d
mokunshao <mokunshao@foxmail.com>
2020-01-07 19:39:55 +0800

    rating 性能优化

--- src/rating/rating.tsx
+++ src/rating/rating.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, HTMLProps, useMemo } from 'react';
 import './rating.scss';
 import { joinedClass } from '../utils/joinedClass';
 import { classes } from '../utils/classes';
@@ -6,41 +6,41 @@ import Star from './star';
 
 export const baseClass = joinedClass('rating');
 
-interface Props{
-  className?: string;
+export interface Props extends Omit<HTMLProps<HTMLDivElement>, 'onChange'> {
   max?: number;
   value: number;
   onChange: (value: number) => void;
   clearable?: boolean;
 }
 
-const Rating: React.FC<Props> = (props) => {
+const Rating: React.FC<Props> = React.memo((props) => {
   const {
     className, max = 5, value, onChange, clearable = false, ...rest
   } = props;
 
   const [override, setOverride] = useState(0);
 
-  const stars = [];
-
-  for (let index = 1; index <= max; index += 1) {
-    stars.push(<Star
-      lighten={index <= (override || value)}
-      index={index}
-      key={index}
-      rating={value}
-      setOverride={setOverride}
-      setRating={onChange}
-      clearable={clearable}
-    />);
-  }
-
+  const stars = useMemo(() => {
+    const array = [];
+    for (let index = 1; index <= max; index += 1) {
+      array.push(<Star
+        lighten={index <= (override || value)}
+        index={index}
+        key={index}
+        rating={value}
+        setOverride={setOverride}
+        setRating={onChange}
+        clearable={clearable}
+      />);
+    }
+    return array;
+  }, [clearable, max, onChange, override, value]);
 
   return (
     <div className={classes(baseClass(), className)} {...rest}>
       {stars}
     </div>
   );
-};
+});
 
 export default Rating;
