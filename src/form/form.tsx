322aa15a481b10f44b54c56dc00177cdd6902aa0
mokunshao <mokunshao@foxmail.com>
2020-01-08 17:21:20 +0800

    Update form.tsx

--- src/form/form.tsx
+++ src/form/form.tsx
@@ -16,11 +16,11 @@ export interface ErrorsMap {
 
 interface Props extends FormHTMLAttributes<HTMLFormElement> {
   values: FormValues;
-  fields: Array<{ name: string; label: string; input: { type: string } }>;
+  fields: Array<{ name: string; label: string; input: { type: string; }; }>;
   buttons: React.ReactFragment;
   onSubmit: React.FormEventHandler;
   onChange: (values: FormValues) => void;
-  errors: { [key: string]: string[] };
+  errors: { [key: string]: string[]; };
   errorsDisplayMode?: 'first' | 'all';
   transformError?: (erorr: string) => string;
 }
